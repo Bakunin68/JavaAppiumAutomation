cacd1cc88763613ef70e79b0a7c868da41136262
Maksim Kotelnikov <117776279+Bakunin68@users.noreply.github.com>
2023-03-31 20:05:03 +0400

    Add files via upload

--- FirstTest.java
+++ FirstTest.java
@@ -2,7 +2,7 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
